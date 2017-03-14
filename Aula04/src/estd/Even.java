325385cfb850aa6e5836fa021227050816c528fe
Gustavo Muchon <gustavo.muchon@univille.br>
2017-03-13 21:25:28 -0300

    Aula 04 21:25

---
+++ Aula04/src/estd/Even.java
@@ -0,0 +1,18 @@
+package estd;
+
+public class Even {
+	public int countEvens(int[] nums){
+		for(int i=0; i<nums.length; i++){
+			System.out.println(nums[i]);
+			
+		}
+		
+		for(int num : nums){	
+			System.out.println();
+			
+		}
+		
+		return 0;
+	}
+
+}
