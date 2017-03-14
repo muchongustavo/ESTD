520e37d1215046c9363e373ae954bf13c799f74c
Gustavo Muchon <gustavo.muchon@univille.br>
2017-03-13 22:11:46 -0300

    Aula 04 22:11

--- Aula04/src/estd/BigDiff.java
+++ Aula04/src/estd/BigDiff.java
@@ -1,5 +1,25 @@
 package estd;
 
 public class BigDiff {
-
+	
+	public int bigDiff(int[] nums) {
+		int maior = nums[0];
+        int menor = nums[0];
+		
+		for(int num: nums){
+			
+			if(num > maior){
+				maior = num;
+			}
+			
+			if(num < menor){
+				menor = num;
+			}
+		}
+		int result = maior - menor;
+		return result;
+		
+	}
 }
+
+
