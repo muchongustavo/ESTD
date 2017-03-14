5be53960e6436df8a52f67d0afe5a878b04d84f8
Gustavo Muchon <gustavo.muchon@univille.br>
2017-03-13 21:48:10 -0300

    Aula 04 21:48

--- Aula04/src/estd/Even.java
+++ Aula04/src/estd/Even.java
@@ -2,17 +2,20 @@ package estd;
 
 public class Even {
 	public int countEvens(int[] nums){
-		for(int i=0; i<nums.length; i++){
-			System.out.println(nums[i]);
-			
-		}
-		
+	//	for(int i=0; i<nums.length; i++){
+		//	System.out.println(nums[i]);	
+	//	}
+		int counter = 0;
 		for(int num : nums){	
-			System.out.println();
+			if(num % 2 ==0){
+				//é par
+				counter ++;
+				
+			}
 			
 		}
 		
-		return 0;
+		return counter;
 	}
 
 }
